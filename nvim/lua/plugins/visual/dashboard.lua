2a5dbf9ac5824cadfbd53f15f986904b67f427a1
tmarek <tiago.marek@adp.com>
2024-01-08 11:43:53 -0300

    update dashboard commands

--- nvim/lua/plugins/visual/dashboard.lua
+++ nvim/lua/plugins/visual/dashboard.lua
@@ -1,55 +1,54 @@
 return {
 
-	"goolord/alpha-nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	priority = 10000,
-	config = function()
-		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.dashboard")
+  'goolord/alpha-nvim',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+  priority = 10000,
+  config = function()
+    local alpha = require 'alpha'
+    local dashboard = require 'alpha.themes.dashboard'
 
-		dashboard.section.header.val = {
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                     ]],
-			[[       ████ ██████           █████      ██                     ]],
-			[[      ███████████             █████                             ]],
-			[[      █████████ ███████████████████ ███   ███████████   ]],
-			[[     █████████  ███    █████████████ █████ ██████████████   ]],
-			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-		}
+    dashboard.section.header.val = {
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                     ]],
+      [[       ████ ██████           █████      ██                     ]],
+      [[      ███████████             █████                             ]],
+      [[      █████████ ███████████████████ ███   ███████████   ]],
+      [[     █████████  ███    █████████████ █████ ██████████████   ]],
+      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+    }
 
-		dashboard.section.buttons.val = {
-			dashboard.button("e", "New file", "<cmd>ene <CR>"),
-			dashboard.button("SPC f f", "Find file"),
-			dashboard.button("SPC ?", "Recently opened files"),
-			dashboard.button("SPC f r", "Resume Telescope"),
-			dashboard.button("SPC f g", "Find word"),
-		}
-		_Gopts = {
-			position = "center",
-			hl = "Type",
-			-- wrap = "overflow";
-		}
+    dashboard.section.buttons.val = {
+      dashboard.button('e', 'New file', '<cmd>ene <CR>'),
+      dashboard.button('f', 'Find file', '<cmd>Telescope find_files<CR>'),
+      dashboard.button('r', 'Recently opened files', '<cmd>Telescope oldfiles<CR>'),
+      dashboard.button('w', 'Find word', '<cmd> Telescope live_grep<CR>'),
+    }
+    _Gopts = {
+      position = 'center',
+      hl = 'Type',
+      -- wrap = "overflow";
+    }
 
-		local function footer()
-			return "Programming is an art of patience not talent"
-		end
+    local function footer()
+      return 'Programming is an art of patience not talent'
+    end
 
-		dashboard.section.footer.val = footer()
+    dashboard.section.footer.val = footer()
 
-		dashboard.opts.opts.noautocmd = true
-		alpha.setup(dashboard.opts)
-	end,
+    dashboard.opts.opts.noautocmd = true
+    alpha.setup(dashboard.opts)
+  end,
 }
