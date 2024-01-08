c1fa08787c77f9659060bc3e292c151043b2219c
tmarek <tiago.marek@adp.com>
2024-01-08 11:44:28 -0300

    add plugins.coding as a plugin folder

--- nvim/lua/install-lazy.lua
+++ nvim/lua/install-lazy.lua
@@ -4,14 +4,14 @@
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
+  vim.fn.system {
     'git',
     'clone',
     '--filter=blob:none',
     'https://github.com/folke/lazy.nvim.git',
     '--branch=stable', -- latest stable release
     lazypath,
-  })
+  }
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -24,11 +24,12 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   { import = 'plugins' },
   { import = 'plugins.ai' },
+  { import = 'plugins.coding' },
   { import = 'plugins.git' },
   { import = 'plugins.lsp' },
   { import = 'plugins.navigation' },
   { import = 'plugins.visual' },
   ui = {
-    border = "double",
+    border = 'double',
   },
 }, {})
