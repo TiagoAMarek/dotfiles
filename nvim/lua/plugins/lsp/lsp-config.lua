1852e2aae543c66708439c7d84d67ee3d527f03e
tmarek <tiago.marek@adp.com>
2024-01-08 11:43:39 -0300

    add typescript-tools plugin and update lsp configs

--- nvim/lua/plugins/lsp/lsp-config.lua
+++ nvim/lua/plugins/lsp/lsp-config.lua
@@ -3,47 +3,34 @@ return {
   {
     'neovim/nvim-lspconfig',
     dependencies = {
-      'nvimtools/none-ls.nvim',
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
       'folke/neodev.nvim',
-      'stevearc/conform.nvim',
     },
     config = function()
       -- Setup neovim lua configuration
       require('neodev').setup()
 
-      require('mason').setup()
+      require('mason').setup {
+        ui = {
+          border = 'rounded',
+          icons = {
+            package_installed = '✓',
+            package_pending = '➜',
+            package_uninstalled = '✗',
+          },
+        },
+      }
       require('mason-lspconfig').setup {
         ensure_installed = { 'tsserver', 'eslint', 'jsonls', 'lua_ls' },
       }
 
       local lspconfig = require 'lspconfig'
-      local null_ls = require 'null-ls'
 
       lspconfig.jsonls.setup {}
-      lspconfig.tsserver.setup {}
+      -- lspconfig.tsserver.setup {}
+      -- lspconfig.eslint.setup {}
       lspconfig.lua_ls.setup {}
-
-      -- add formatting feature
-      require('conform').setup {
-        -- Map of filetype to formatters
-        formatters_by_ft = {
-          lua = { 'stylua' },
-          -- Conform will run multiple formatters sequentially
-          -- Use a sub-list to run only the first available formatter
-          javascript = { { 'eslint_d', 'eslint' } },
-          typescript = { { 'eslint_d', 'eslint' } },
-          json = { { 'eslint_d', 'eslint' } },
-        },
-      }
-
-      null_ls.setup {
-        sources = {
-          null_ls.builtins.code_actions.eslint_d,
-          null_ls.builtins.diagnostics.eslint_d,
-        },
-      }
 
       -- keymaps
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'Hover Documentation' })
@@ -53,11 +40,9 @@ return {
       vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, { desc = 'Go to type definition' })
       vim.keymap.set('n', 'gr', vim.lsp.buf.references, { desc = 'Go to references' })
       vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, { desc = 'Go to signature help' })
-      vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, { desc = 'Rename' })
+      vim.keymap.set('n', '<leader>cr', vim.lsp.buf.rename, { desc = 'Rename' })
       vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code Action' })
-      vim.keymap.set('n', '<leader>cf', function(bufnr)
-        require('conform').format { bufnr = bufnr }
-      end, { desc = 'Format' })
+      vim.keymap.set('n', '<leader>cf', vim.lsp.buf.format, { desc = 'Format' })
     end,
   },
 }
