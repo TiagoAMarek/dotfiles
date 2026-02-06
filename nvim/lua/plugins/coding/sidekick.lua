2fdb6129bd48da23d68470579c2ea7f6c531b479
tmarek <tmarek@rocketlaywer.com.br>
2026-02-06 15:30:57 -0300

    update configs

--- nvim/lua/plugins/coding/sidekick.lua
+++ nvim/lua/plugins/coding/sidekick.lua
@@ -13,5 +13,28 @@ return {
         },
       },
     },
+    keys = {
+      {
+        "<leader>al",
+        function()
+          require("sidekick.cli").send({ msg = "{line}" })
+        end,
+        desc = "Send Line to Sidekick",
+      },
+      {
+        "<leader>ad",
+        function()
+          require("sidekick.cli").send({ msg = "{diagnostics}" })
+        end,
+        desc = "Send Diagnostics to Sidekick",
+      },
+      {
+        "<leader>ab",
+        function()
+          require("sidekick.cli").send({ msg = "{buffers}" })
+        end,
+        desc = "Send Buffers to Sidekick",
+      },
+    },
   },
 }
